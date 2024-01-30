cb660a52ff99102c0e8cbf7cdf5240995254088f
Kazunori Kimura <kazunori.kimura.js@gmail.com>
2024-01-30 17:08:45 +0900

    Initial Commit

---
+++ MsalSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MsalSample
+//
+//  Created by Kazunori Kimura on 2024/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MsalSample/MsalSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MsalSampleApp.swift
+//  MsalSample
+//
+//  Created by Kazunori Kimura on 2024/01/30.
+//
+
+import SwiftUI
+
+@main
+struct MsalSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
